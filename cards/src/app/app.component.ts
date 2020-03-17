dabf4fe7efb3df7d74aa4d74806b071d8a195bae
byariel95 <ariel@dooply.net>
2020-03-17 08:43:35 -0600

    data binding and ngif

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cards';
+  title = 'store';
+  num1 = '';
+  num2 = '';
+ 
 }

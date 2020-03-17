aa96f690ad4bb07e6c03c810eb8ed21300e4c4d3
byariel95 <ariel@dooply.net>
2020-03-17 14:51:06 -0600

    ngfor in object

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product} from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,53 @@ export class AppComponent {
   num2 = '';
   nombres = ['Ariel', 'jose ', 'miguel', 'pedro'];
   name = '';
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/kisspng.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/kisspng.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/kisspng.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/kisspng.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/kisspng.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/images/kisspng.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+  ];
+
+
   addName(name: string){
     this.nombres.push(name);
   }

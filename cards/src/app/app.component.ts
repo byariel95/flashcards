16321470c5eaf851cbe6a704b7cca05bd665fc1e
byariel95 <ariel@dooply.net>
2020-03-18 16:05:56 -0600

    inputs and outputs

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Product} from './product.model';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +12,51 @@ export class AppComponent {
   num2 = '';
   nombres = ['Ariel', 'jose ', 'miguel', 'pedro'];
   name = '';
+  total = 0;
+   products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/kisspng.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/kisspng.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/kisspng.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/kisspng.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/kisspng.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/images/kisspng.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+  ];
 
   addName(name: string){
     this.nombres.push(name);
@@ -19,4 +64,10 @@ export class AppComponent {
   deleteName(index: number){
     this.nombres.splice(index, 1);
   }
+
+  addecart(id: any){
+     this.total += 1;
+     console.log(id);
+  }
+
 }
--- cards/src/app/components/product/product.component.ts
+++ cards/src/app/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter} from '@angular/core';
 import { Product } from '../../product.model';
 
 @Component({
@@ -8,49 +8,11 @@ import { Product } from '../../product.model';
 
 export class ProductComponent {
 
-    products: Product[] = [
-        {
-          id: '1',
-          image: 'assets/images/kisspng.png',
-          title: 'Camiseta',
-          price: 80000,
-          description: 'bla bla bla bla bla'
-        },
-        {
-          id: '2',
-          image: 'assets/images/kisspng.png',
-          title: 'Hoodie',
-          price: 80000,
-          description: 'bla bla bla bla bla'
-        },
-        {
-          id: '3',
-          image: 'assets/images/kisspng.png',
-          title: 'Mug',
-          price: 80000,
-          description: 'bla bla bla bla bla'
-        },
-        {
-          id: '4',
-          image: 'assets/images/kisspng.png',
-          title: 'Pin',
-          price: 80000,
-          description: 'bla bla bla bla bla'
-        },
-        {
-          id: '5',
-          image: 'assets/images/kisspng.png',
-          title: 'Stickers',
-          price: 80000,
-          description: 'bla bla bla bla bla'
-        },
-        {
-          id: '6',
-          image: 'assets/images/kisspng.png',
-          title: 'Stickers',
-          price: 80000,
-          description: 'bla bla bla bla bla'
-        },
-      ];
-
+    @Input() prod: Product;
+    @Output() addedProduct: EventEmitter<any> = new EventEmitter(); // definir tipo de dato
+     
+    addCar() {
+      console.log('se ha agregado');
+      this.addedProduct.emit(this.prod.id);
+    }
 }

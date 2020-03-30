84140461fb460478e54d99c59ae65cf6b4543eab
byariel95 <ariel@dooply.net>
2020-03-30 17:04:41 -0600

    lazy loading and moduling home component

--- cards/src/app/app.module.ts
+++ cards/src/app/app.module.ts
@@ -7,13 +7,11 @@ import { ProductComponent} from './components/product/product.component';
 import { CartComponent } from './components/cart/cart.component';
 import { ExponentialPipe } from './exponential.pipe';
 import { HighlightDirective } from './highlight.directive';
-import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ProductsComponent } from './components/products/products.component';
 import { DemoComponent } from './components/demo/demo.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { BannerComponent } from './components/banner/banner.component';
 import { PageErrorComponent } from './components/page-error/page-error.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { LayoutComponent } from './components/layout/layout.component';
@@ -25,13 +23,11 @@ import { LayoutComponent } from './components/layout/layout.component';
     CartComponent,
     ExponentialPipe,
     HighlightDirective,
-    HomeComponent,
     ContactComponent,
     ProductsComponent,
     DemoComponent,
     HeaderComponent,
     FooterComponent,
-    BannerComponent,
     PageErrorComponent,
     ProductDetailComponent,
     LayoutComponent

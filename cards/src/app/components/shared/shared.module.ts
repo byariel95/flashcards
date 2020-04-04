c427bbb6050e438ff9f76e9e4cb02d91969f2a50
byariel95 <ariel@dooply.net>
2020-04-03 18:50:28 -0600

    created nav}

--- cards/src/app/components/shared/shared.module.ts
+++ cards/src/app/components/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
 import { HighlightDirective } from './directives/highlight/highlight.directive';
+import {MaterialModule} from '../material/material.module';
 
 
 
@@ -23,7 +24,9 @@ import { HighlightDirective } from './directives/highlight/highlight.directive';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MaterialModule
+
   ]
 })
 export class SharedModule { }
